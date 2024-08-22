1c198fd15a4e1a0898d97864ab8b84f57322de1c
talisu <thalyssondelanopr@gmail.com>
2024-08-22 13:51:48 -0300

    feat: complet playnumber

--- playNumber/Main.py
+++ playNumber/Main.py
@@ -1,5 +1,5 @@
-import random
-import os\
+
+import Vetor_utils
 
 def main():
 
@@ -8,13 +8,11 @@ def main():
     match OpcaoMenuInicial:
 
         case 1:
-            Valores = GerarDadosAutomaticos()
-            print(f"\n{Valores}")
+            Valores = Vetor_utils.GerarDadosAutomaticos()
         case 2:
-            Valores = InserirManualmente()
-            print(f"\n{Valores}")
+            Valores = Vetor_utils.InserirManualmente()
         case 3:
-            Valores = InserirArquivo()
+            Valores = Vetor_utils.InserirArquivo()
             print(f"\n{Valores}")
     
     MenuGeral()
@@ -25,48 +23,57 @@ def main():
         match OpcoesMenuGeral:
 
             case 1:
-                MostrarValores(Valores)
+                Vetor_utils.MostrarValores(Valores)
             case 2:
-                pass
+                Valores = Vetor_utils.Resetar(Valores)
             case 3:
-                QuantidadeVetores(Valores)
+                Vetor_utils.QuantidadeVetores(Valores)
             case 4:
-                MaiorMenorVetor(Valores)
+                Vetor_utils.MaiorMenorVetor(Valores)
             case 5:
-                SomatorioValores(Valores)
+                Vetor_utils.SomatorioValores(Valores)
             case 6:
-                MediaValores(Valores)
+                Vetor_utils.MediaValores(Valores)
             case 7:
-                QtdValoresPositivos(Valores)
+                Vetor_utils.QtdValoresPositivos(Valores)
             case 8:
-                QtdValoresNegativos(Valores)
+                Vetor_utils.QtdValoresNegativos(Valores)
             case 9:
-                Regras(Valores)
+                Valores = Vetor_utils.Regras(Valores)
             case 10:
-                AddNovosValores(Valores)
-                pass
+                Vetor_utils.AddNovosValores(Valores)
             case 11:
-                Valores = RemoverPorValor(Valores)
-                print(Valores)
+                Valores = Vetor_utils.RemoverPorValor(Valores)
             case 12:
-                Valores = RemoverPorIndex(Valores)
-                print(Valores)
-                pass
+                Valores = Vetor_utils.RemoverPorIndex(Valores)
             case 13:
-                EditarPorPosicao(Valores)
-                pass
+                Vetor_utils.EditarPorPosicao(Valores)
             case 14:
-                #SalvarArquivo()
-                pass
+                Vetor_utils.SalvarArquivo(Valores)
 
         input("\n| Pressione Enter para CONTINUAR: |")
-        clear_screen()
+        Vetor_utils.clear_screen()
         MenuGeral()
         OpcoesMenuGeral = int(input("> "))
-            
-            
+    
+    if OpcoesMenuGeral == 15:
+        print("\nEncerrando processamento...")
+
+        opcoes = input("\nDeseja sair sem salvar(Y/N)?")
+
+        while opcoes != "Y" and opcoes != "N":
+            opcoes = input("\nDeseja sair sem salvar(Y/N)?")
+
+        if opcoes == "N":
+            Vetor_utils.SalvarArquivo(Valores)
+        else:
+            print("\nFim...")
+            return
 
 def MenuGeral():
+
+    Vetor_utils.clear_screen()
+
     opcoes = """
 ======================================
         >>> Menu PlayNumber <<<
@@ -102,277 +109,5 @@ def Menu():
 """
     return opcao_menu
 
-def GerarDadosAutomaticos():
-
-    tamanho = int(input("\nInsira o Número de dados > "))
-    min_ = int(input("\nInsira o Menor valor onde os dados se encontram > "))
-    max_ = int(input("\nInsira o Maior valor onde os dados se encontram > "))
-
-    ListaDados = []
-
-    for i in range(tamanho):
-        ListaDados.append(random.randint(min_,max_))
-
-    return ListaDados
-
-def InserirManualmente():
-
-    ListaDados = []
-
-    tamanho = int(input("\nInsira o dado > "))
-
-    for elements in range(tamanho):
-
-        Dados = int(input(f"Dado [{elements + 1}] > "))
-        ListaDados.append(Dados)
-    
-    return ListaDados
-     
-
-def InserirArquivo():
-
-    NomeArquivo = input("Insira o Nome do arquivo > ")
-    Arquivo = open(f"{NomeArquivo}").split()
-
-def MostrarValores(valores):
-    print(valores)
-
-def QuantidadeVetores(valores):
-
-    qtdVetores = 0
-    
-    for elements in range(len(valores)):
-        qtdVetores += 1
-
-    print(f"\nA quantidade de vetores presentes é {qtdVetores} unidades")
-
-def MaiorMenorVetor(valores):
-
-    MenorVetor = valores[0]
-    MaiorVetor = 0
-
-    contador = 0
-
-    posicaoMenorVetor = 0
-    posicaoMaiorVetor = 0
-
-    for elements in valores:
-        if elements > MaiorVetor:
-            MaiorVetor = elements
-            posicaoMaiorVetor = contador + 1 
-        if elements < MenorVetor:
-            MenorVetor = elements
-            posicaoMenorVetor = contador + 1
-        
-        contador += 1
-
-    print(f"""
-====================================
-| >> Maior Valor: {MaiorVetor} na {posicaoMaiorVetor}° Posição |
-| >> Menor Valor: {MenorVetor} na {posicaoMenorVetor}° Posição |
-====================================
-""")
-    
-def SomatorioValores(valores):
-
-    somatorio = 0
-
-    for elements in valores:
-        somatorio += elements
-    
-    print(f"\nO somatorio dos valores é {somatorio}")   
-
-def MediaValores(valores):
-
-    somatorio = 0
-    qtdValores = 0
-
-    for elements in valores:
-        somatorio += elements
-        qtdValores += 1
-    
-    media = somatorio / qtdValores
-
-    print(f"|A média dos Valores >> {media}|")
-
-    
-def QtdValoresPositivos(valores):
-
-    ListaPositivos = []
-
-    contadorPositivos = 0
-
-    for elements in valores:
-        if elements >= 0:
-            ListaPositivos.append(elements)
-            contadorPositivos += 1
-
-    print(f"""
-======================
-{ListaPositivos}
-
-|Quantidade >> {contadorPositivos}|
-======================
-""")
-
-def QtdValoresNegativos(valores):
-        
-    ListaNegativos = []
-
-    contadorNegativos = 0
-
-    for elements in valores:
-        if elements >= 0:
-            ListaNegativos.append(elements)
-            contadorNegativos += 1
-
-    print(f"""
-======================
-{ListaNegativos}
-
-|Quantidade >> {contadorNegativos}|
-======================
-""")
-
-def Regras(valores):
-    opcoes = """
-======================================
-    >>> Atualizando Valores <<<
-======================================
-| 1 | - Multiplicar Por Um Valor
-| 2 | - Elevar a Um Valor
-| 3 | - Reduzir a uma fração
-| 4 | - Substituir valores negativos por um número aleatórios da uma faixa(min, max)
-| 5 | - Ordenar valores
-| 6 | - Embaralhar valores
-======================================
-"""
-
-    print(opcoes)
-    opcoesMenu = int(input("> "))
-
-    # clear_screen()
-    
-    match opcoesMenu:
-        case 1:
-            MultiplicarValores(valores)
-        case 2: 
-            ElevarValor(valores)
-            pass
-        case 3:
-            ReduzirAhFracao(valores)
-            pass
-        case 4:
-            # SubstituirValoresNegativos(valores)
-            pass
-        case 5:
-            # OrdenarValores(valores)
-            pass
-        case 6:
-            # EmbaralharValores(valores)
-            pass
-        
-
-
-def clear_screen():
-    # Verifica o sistema operacional
-    if os.name == 'posix':  # Linux/macOS
-        os.system('clear')
-    elif os.name == 'nt':   # Windows
-        os.system('cls')
-
-def MultiplicarValores(valores):
-
-    #Reseter Valores para sua forma original
-
-    valor = int(input('Informe um valor para os termos serem multiplicados > '))
-
-    for elements in range(len(valores)):
-        valores[elements] = valores[elements] * valor
-
-    print(valores)
-
-def ElevarValor(valores):
-
-    #Reseter Valores para sua forma original
-
-    valor = int(input('Informe um valor para os termos serem Elevados > '))
-
-    for elements in range(len(valores)):
-        valores[elements] = valores[elements] ** valor
-
-    print(valores)
-
-def ReduzirAhFracao(valores):
-    pass
-
-def AddNovosValores(valores):
-    
-    opcoes = """
-======================================
-    >>> Adicionando Valores <<<
-======================================
-| 1 | - Inicializar Dados Automaticos
-| 2 | - Inserir Valores Manualmente
-| 3 | - Inserir Arquivo
-============================
-"""
-    print(opcoes)
-
-    entrada = int(input("\n> "))
-
-    match entrada:
-        case 1:
-            valores += GerarDadosAutomaticos()
-        case 2:
-            valores += InserirManualmente()
-        case 3:
-            valores += InserirArquivo()
-
-def RemoverPorValor(valores):
-
-    valor = int(input("Informe o valor a ser excluido > "))
-
-    ListaNova = []
-
-    for elements in valores:
-        if elements == valor:
-            continue
-        
-        ListaNova.append(elements)
-
-    valores = ListaNova
-
-    return valores
-    
-def RemoverPorIndex(valores):
-
-    valor = int(input("Informe o index a ser excluido > "))
-
-    ListaNova = []
-
-    for elements in range(len(valores)):
-        if elements + 1 == valor:
-            continue
-            
-        ListaNova.append(valores[elements])
-
-    valores = ListaNova
-
-    return valores
-
-def EditarPorPosicao(valores):
-
-    indexEditado = int(input("Informe o index a ser editado > "))
-    valorInserido = int(input("Informe o valor a ser Inserido > "))
-
-    valores[indexEditado - 1] = valorInserido
-
-    print(f"O valor {valores[indexEditado]} foi inserido no Posição [{indexEditado}]!!")
-
-    print(f"\nA nova Lista de vetores Consiste em {valores}")
-
 main()
-
-
 
